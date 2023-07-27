d29017e3706001e4ebfc1ccdad4455cb8612f7dd
davidcoats <david@getsafetycone.com>
2023-07-27 01:31:36 -0700

    Interim changes.

--- source/R5T.O0006/Code/Values/IProjectFileOperations.cs
+++ source/R5T.O0006/Code/Values/IProjectFileOperations.cs
@@ -171,6 +171,64 @@ namespace R5T.O0006
             return output;
         }
 
+        /// <inheritdoc cref="F0016.F001.IProjectReferencesOperator.GetAllRecursiveProjectReferences(IEnumerable{string})"/>
+        public async Task<IProjectFilePath[]> Get_RecursiveProjectReferences(
+            T0159.ITextOutput textOutput,
+            IEnumerable<IProjectFilePath> projectFilePaths)
+        {
+            var recursiveProjectReferences = await Instances.ProjectReferencesOperator.GetAllRecursiveProjectReferences(
+                projectFilePaths.Get_Values());
+
+            var output = recursiveProjectReferences.To_Typeds(x => x.ToProjectFilePath()).Now();
+            return output;
+        }
+
+        /// <inheritdoc cref="F0016.F001.IProjectReferencesOperator.GetAllRecursiveProjectReferences(IEnumerable{string})"/>
+        public Task<IProjectFilePath[]> Get_RecursiveProjectReferences(
+            T0159.ITextOutput textOutput,
+            params IProjectFilePath[] projectFilePaths)
+        {
+            return this.Get_RecursiveProjectReferences(
+                textOutput,
+                projectFilePaths.AsEnumerable());
+        }
+
+        /// <summary>
+        /// Inclusive, in that the input project files are included in the output.
+        /// <inheritdoc cref="F0016.F001.IProjectReferencesOperator.GetAllRecursiveProjectReferences(IEnumerable{string})"/>
+        /// </summary>
+        public async Task<IProjectFilePath[]> Get_RecursiveProjectReferences_Inclusive(
+            IProjectFilePath projectFilePath)
+        {
+            var recursiveProjectReferences = await this.Get_RecursiveProjectReferences(projectFilePath);
+
+            var output = recursiveProjectReferences.Append(projectFilePath).Now();
+            return output;
+        }
+
+        /// <inheritdoc cref="Get_RecursiveProjectReferences_Inclusive(IProjectFilePath)"/>
+        public async Task<IProjectFilePath[]> Get_RecursiveProjectReferences_Inclusive(
+            T0159.ITextOutput textOutput,
+            IEnumerable<IProjectFilePath> projectFilePaths)
+        {
+            var recursiveProjectReferences = await this.Get_RecursiveProjectReferences(
+                textOutput,
+                projectFilePaths);
+
+            var output = recursiveProjectReferences.Union(projectFilePaths).Now();
+            return output;
+        }
+
+        /// <inheritdoc cref="Get_RecursiveProjectReferences_Inclusive(IProjectFilePath)"/>
+        public Task<IProjectFilePath[]> Get_RecursiveProjectReferences_Inclusive(
+            T0159.ITextOutput textOutput,
+            params IProjectFilePath[] projectFilePaths)
+        {
+            return this.Get_RecursiveProjectReferences_Inclusive(
+                textOutput,
+                projectFilePaths.AsEnumerable());
+        }
+
         /// <inheritdoc cref="F0016.F001.IProjectReferencesOperator.Get_RecursiveProjectReferences_InDependencyOrder(IEnumerable{string})"/>
         public async Task<IProjectFilePath[]> Get_RecursiveProjectReferences_InDependencyOrder(
             IProjectFilePath projectFilePath)
